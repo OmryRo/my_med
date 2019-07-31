81252c3b0f6a000fa94037ae60a4bfac37549c70
Omry Rosenberg <omry99@gmail.com>
2019-07-31 19:20:59 +0300

    updated the style of the appointments page.

--- app/src/main/java/il/ac/huji/cs/postpc/mymeds/AppointmentsFragment.java
+++ app/src/main/java/il/ac/huji/cs/postpc/mymeds/AppointmentsFragment.java
@@ -6,19 +6,84 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class AppointmentsFragment extends Fragment {
 
     private AppointmentsFragmentListener listener;
+    private FloatingActionButton newDoctorFab;
+    private RecyclerView recyclerView;
+    private ReminderFragment reminderFragment;
 
     public AppointmentsFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_appointments, container, false);
+        View view = inflater.inflate(R.layout.fragment_appointments, container, false);
+        newDoctorFab = view.findViewById(R.id.doctors_add_fab);
+        recyclerView = view.findViewById(R.id.doctors_container);
+
+        reminderFragment = new ReminderFragment();
+        reminderFragment.init(
+                "You didn't visit Dr. Palony in the last 3 months.",
+                new ReminderFragment.ReminderFragmentListener() {
+                    @Override
+                    public void onRemindMeLaterClicked() {
+
+                    }
+
+                    @Override
+                    public void onDismissClicked() {
+
+                    }
+                }
+        );
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.appointments_reminder_container, reminderFragment)
+                .commit();
+
+        newDoctorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        recyclerView.setAdapter(new RecyclerView.Adapter<ListItemHolder>() {
+            @NonNull
+            @Override
+            public ListItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return ListItemHolder.createHolder(parent);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull ListItemHolder holder, int position) {
+                holder.setData(
+                        R.drawable.ic_user_md_solid,
+                        "Dr. Palony Almony",
+                        "Appointment at Friday 10:00."
+                );
+            }
+
+            @Override
+            public int getItemCount() {
+                return 10;
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+        return view;
     }
 
     @Override
@@ -30,12 +95,12 @@ public class AppointmentsFragment extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        listener = null;
     }
 
     public interface AppointmentsFragmentListener {}
