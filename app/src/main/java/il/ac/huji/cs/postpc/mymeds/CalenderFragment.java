112d5d5d3a31e54e6eb5c36631a51b185af0059f
Omry Rosenberg <omry99@gmail.com>
2019-07-31 19:39:29 +0300

    added the basic design for the calender page

--- app/src/main/java/il/ac/huji/cs/postpc/mymeds/CalenderFragment.java
+++ app/src/main/java/il/ac/huji/cs/postpc/mymeds/CalenderFragment.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,13 +18,41 @@ import android.view.ViewGroup;
 public class CalenderFragment extends Fragment {
 
     private CalenderFragmentListener listener;
+    private RecyclerView recyclerView;
 
     public CalenderFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calender, container, false);
+        View view = inflater.inflate(R.layout.fragment_calender, container, false);
+        recyclerView = view.findViewById(R.id.event_container);
+
+        recyclerView.setAdapter(new RecyclerView.Adapter<ListItemHolder>() {
+            @NonNull
+            @Override
+            public ListItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return ListItemHolder.createHolder(parent);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull ListItemHolder holder, int position) {
+                holder.setData(
+                        R.drawable.ic_user_md_solid,
+                        "Appointment to Dr. Palony Almony",
+                        "18:00-19:00"
+                );
+            }
+
+            @Override
+            public int getItemCount() {
+                return 4;
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+        return view;
     }
 
     @Override
