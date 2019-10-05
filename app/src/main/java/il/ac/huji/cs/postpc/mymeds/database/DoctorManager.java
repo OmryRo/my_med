a882027c436f9f0ec716f6374278e8f2e34a3281
abuhay.abune <abuhay.abune@cs.huji.ac.il>
2019-10-05 13:49:46 +0300

    add json file for demo

--- app/src/main/java/il/ac/huji/cs/postpc/mymeds/database/DoctorManager.java
+++ app/src/main/java/il/ac/huji/cs/postpc/mymeds/database/DoctorManager.java
@@ -1,7 +1,14 @@
 package il.ac.huji.cs.postpc.mymeds.database;
 
 import android.content.Context;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,14 +17,15 @@ import il.ac.huji.cs.postpc.mymeds.database.controllers.AppDatabase;
 import il.ac.huji.cs.postpc.mymeds.database.entities.Doctor;
 
 public class DoctorManager {
-
+    private static final String TAG = DoctorManager.class.getSimpleName();
     private final AppDatabase db;
     private List<Doctor> doctors;
     private Map<Long, Doctor> dbMap;
+    private Context context;
 
     public DoctorManager(Context context) {
         db = AppDatabase.getInstance(context);
-
+        this.context = context;
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -27,15 +35,47 @@ public class DoctorManager {
                 for (Doctor doctor : doctors) {
                     dbMap.put(doctor.id, doctor);
                 }
+                if (doctors.size() == 0) {
+                    try {
+                        JSONArray jsonArray = new JSONArray(readJSONFromAsset());
+                        String name, note, phone, email, address;
+                        for (int i = 0; i < jsonArray.length(); ++i) {
+                            name = jsonArray.getJSONObject(i).getString("name");
+                            note = jsonArray.getJSONObject(i).getString("note");
+                            phone = jsonArray.getJSONObject(i).getString("phone");
+                            email = jsonArray.getJSONObject(i).getString("email");
+                            address = jsonArray.getJSONObject(i).getString("address");
+                            add(name, note, phone, email, address);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
 
-                // remove later for debugging propose...
-//                if (doctors.size() == 0) {
-//                    add("Palony Almony the first", "Do something", "+9722222222", "first@docdocdoc.com", "");
-//                    add("Palony Almony the second", "Do something", "+9722222222", "second@docdocdoc.com", "");
-//                    add("Palony Almony the third", "Do something", "+9722222222", "third@docdocdoc.com", "");
-//                }
+                }
+
             }
         }).start();
+    }
+
+    private String convertToString(InputStream is) throws IOException {
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        int totalNumberOfBytesReadIntoTheBuffer = is.read(buffer);
+        Log.d(TAG, "convertToString: read number" + totalNumberOfBytesReadIntoTheBuffer);
+        is.close();
+        return new String(buffer, StandardCharsets.UTF_8);
+    }
+
+    private String readJSONFromAsset() {
+        String result;
+        try {
+            InputStream is = context.getAssets().open("doctors.json");
+            result = convertToString(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return result;
     }
 
     public Doctor add(String name, String note, String phone, String email, String address) {
@@ -48,7 +88,12 @@ public class DoctorManager {
         return doctor;
     }
 
-    public void add(final String name, final String note, final String phone, final String email, final String address, final Listener callback) {
+    public void add(final String name,
+                    final String note,
+                    final String phone,
+                    final String email,
+                    final String address,
+                    final Listener callback) {
         new Thread(new Runnable() {
             @Override
             public void run() {
